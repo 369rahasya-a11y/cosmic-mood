5e4828c0be2d37024e7070c446c73fe548a3ca0c
369rahasya-a11y <369rahasya@gmail.com>
2026-05-28 20:02:46 +0530

    Update generate.py

--- generate.py
+++ generate.py
@@ -33,36 +33,11 @@ print("CONNECTED")
 
 SIGNS = [
     "Aries",
-    "Taurus",
-    "Gemini",
-    "Cancer",
-    "Leo",
-    "Virgo",
-    "Libra",
-    "Scorpio",
-    "Sagittarius",
-    "Capricorn",
-    "Aquarius",
-    "Pisces"
-]
+   ]
 
 MOODS = [
     "Ambitious",
-    "Adventurous",
-    "Creative",
-    "Rebellious",
-    "Confident",
-    "Anxious",
-    "Sad",
-    "Lonely",
-    "Romantic",
-    "Nostalgic",
-    "Exhausted",
-    "Lazy",
-    "Peaceful",
-    "Daydreamy",
-    "Irritated"
-]
+   ]
 
 # =========================
 # LOAD PROMPT
